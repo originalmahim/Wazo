938f145c193a7e2230ce80890c90d1539a5a5dd3
originalmahim <tareqazizmahim@gmail.com>
2023-10-17 20:58:02 +0600

    Created Footer Section Successfully

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,237 @@
+import { FaFacebook, FaGithub, FaTwitter } from 'react-icons/fa'; 
 
-
-const Footer = () => {
-          return (
-          <div>
-           <h1>This Is Footer</h1>                   
+function Footer() {
+  return (
+    <div className="relative mt-16 bg-[#1c6e5f]">
+    
+    <div className="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
+        <div className="md:max-w-md lg:col-span-2">
+          <a
+            href="/"
+            aria-label="Go home"
+            title="Company"
+            className="inline-flex items-center"
+          >
+            
+            <span className="ml-2 text-xl font-bold tracking-wide text-white uppercase">
+            {/* Event Crafter */}
+            </span>
+          </a>
+          <div className="mt-4 lg:max-w-sm">
+            <p className="text-sm text-white">
+            
+            </p>
+            <p className="mt-4 text-sm text-white">
+            
+            </p>
           </div>
-          );
-};
+        </div>
+        <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
+          <div>
+            <p className="font-semibold tracking-wide text-teal-accent-400">
+              Category
+            </p>
+            <ul className="mt-2 space-y-2">
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  News
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  World
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Games
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  References
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className='hidden lg:grid md:grid'>
+            <p className="font-semibold tracking-wide text-teal-accent-400">
+              Cherry
+            </p>
+            <ul className="mt-2 space-y-2">
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Web
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  eCommerce
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Business
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Entertainment
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Portfolio
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className='hidden lg:grid md:grid' >
+            <p className="font-semibold tracking-wide text-teal-accent-400">
+              Apples
+            </p>
+            <ul className="mt-2 space-y-2">
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Media
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Brochure
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Nonprofit
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Educational
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Projects
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div >
+            <p className="font-semibold tracking-wide text-teal-accent-400">
+              Business
+            </p>
+            <ul className="mt-2 space-y-2">
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Infopreneur
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Personal
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Wiki
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="transition-colors duration-300 text-white hover:text-teal-accent-400"
+                >
+                  Forum
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col justify-between pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
+        <p className="text-sm text-white">
+          © Copyright 2023 Event Crafter Inc. All rights reserved.
+        </p>
+        <div className="flex items-center mt-4 space-x-4 sm:mt-0">
+
+        <a
+            href="/"
+            className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+          >
+            <FaGithub className='text-2xl'></FaGithub>
+          </a>
+
+          <a
+            href="/"
+            className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+          >
+            <FaTwitter className='text-2xl'></FaTwitter>
+          </a>
+          <a
+            href="/"
+            className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+          >
+            <FaFacebook className='text-2xl'></FaFacebook>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
 
 export default Footer;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,42 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 
 
 const Header = () => {
+          const links = <>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/applied">Add Product</NavLink></li>
+          <li><NavLink to="/blogs">My Cart</NavLink></li>
+          </>
+
           return (
-          <div>
-          <div className="max-w-7xl mx-auto">
-           <h1 className="text-center text-blue-500" >This Is Header</h1>
+          <div >
+
+          <div className="navbar bg-base-200 max-w-7xl mx-auto">
+          <div className="navbar-start">
+          <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          {links}
+          </ul>
           </div>
-           <Outlet></Outlet>
-           <Footer></Footer>                   
+          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          </div>
+          <div className="navbar-end">
+          <div className="hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+          {links}
+          </ul>
+          </div>
+          <a className="btn">Login</a>
+          </div>
+          </div>
+          <div className="max-w-7xl mx-auto px-4 lg:min-h-[70vh]">
+          <Outlet></Outlet>
+          </div>
+          <Footer></Footer>
           </div>
           );
 };
