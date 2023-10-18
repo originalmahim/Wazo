6aaaf3db8393b5abb65b0fd79d24701e39cc6fb3
originalmahim <tareqazizmahim@gmail.com>
2023-10-18 08:15:36 +0600

    Created Private Raute Section for protect contents only visible for loged in users

--- src/Components/PrivateRaute/PrivateRaute.jsx
+++ src/Components/PrivateRaute/PrivateRaute.jsx
@@ -1,10 +1,26 @@
+import {  useContext } from "react";
+import { AuthContex } from "../../Contex/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import PropTypes from 'prop-types';
 
-const PrivateRaute = () => {
-          return (
-          <div>
-                              
-          </div>
-          );
+const PrivateRaute = ({children}) => {
+
+          const {user,loader} = useContext(AuthContex)
+          const location = useLocation()
+
+          if (loader) {
+          return <h1 className="text-center text-4xl text-pink-400 items-center lg:mt-96">Loading...</h1>
+          }
+           
+          if (user) {
+            return children        
+          }
+
+          return <Navigate state={location.pathname} to = "/login" ></Navigate>
 };
+
+PrivateRaute.propTypes = {
+          children: PropTypes.node
+}
 
 export default PrivateRaute;
